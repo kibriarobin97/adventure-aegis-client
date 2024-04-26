353441c6c5b708d269408bf5105571670acc926e
kibriarobin97 <kibriarobin97@gmail.com>
2024-04-27 02:34:28 +0600

    added my list table

--- src/pages/MyList.jsx
+++ src/pages/MyList.jsx
@@ -1,9 +1,25 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../AuthProvider/AuthProvider";
 
 
 const MyList = () => {
+
+    const {user} = useContext(AuthContext)
+
+    const [spots, setSpots] = useState()
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/mySpots/${user?.email}`)
+        .then(res => res.json())
+        .then(data => {
+            setSpots(data)
+        })
+    },[user])
+
+
     return (
         <div className="min-h-[calc(100vh-353px)]">
-            <h3 className="text-3xl font-bold text-center my-3">My Added Tourists Spots</h3>
+            <h3 className="text-3xl font-bold text-center my-3">My Added Tourists Spots {spots.length}</h3>
             <div className="container p-2 mx-auto sm:p-4 text-gray-700">
                 <div className="overflow-x-auto">
                     <table className="min-w-full text-xs">
@@ -25,28 +41,30 @@ const MyList = () => {
                                 <th className="p-3 text-white">Delete</th>
                             </tr>
                         </thead>
-                        <tbody>
+                        {
+                            spots.map((spot, idx) => <tbody key={spot._id}>
                             <tr className="border-b border-opacity-20 border-gray-700 bg-gray-100">
                                 <td className="p-3">
-                                    <p className="font-semibold">97412378923</p>
+                                    <p className="font-semibold">{idx+1}</p>
                                 </td>
                                 <td className="p-3">
-                                    <p className="font-semibold">Microsoft Corporation</p>
+                                    <p className="font-semibold">{spot?.name}</p>
                                 </td>
                                 <td className="p-3">
-                                    <p className="font-semibold">Friday</p>
+                                    <p className="font-semibold">{spot?.country}</p>
                                 </td>
                                 <td className="p-3">
-                                    <p className="font-semibold">Tuesday</p>
+                                    <p className="font-semibold">{spot?.time}</p>
                                 </td>
                                 <td className="p-3">
-                                    <p className="font-semibold">$15,792</p>
+                                    <button className="font-semibold">Update</button>
                                 </td>
                                 <td className="p-3">
-                                        <p className="font-semibold">Pending</p>
+                                    <button className="font-semibold">Delete</button>
                                 </td>
                             </tr>
-                        </tbody>
+                        </tbody>)
+                        }
                     </table>
                 </div>
             </div>
